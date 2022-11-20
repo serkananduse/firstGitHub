c5ba5de1c76660326157ddba69816519f0af3f27
serkananduse <117192750+serkananduse@users.noreply.github.com>
2022-11-20 17:31:02 +0100

    Delete src directory

--- src/A02.java
+++
@@ -1,7 +0,0 @@
-public class A02 {
-    public static void main(String[] args) {
-        System.out.println("helooooo serkaaaannnn");
-
-    }
-
-}
--- src/A3.java
+++
@@ -1,5 +0,0 @@
-public class A3 {
-    public static void main(String[] args) {
-
-    }
-}
